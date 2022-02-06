201ae8f1365a700f4ec2e49dfad4ab7121e75ec4
Tasondock Durin <temgoua484@gmail.com>
2022-02-06 14:45:59 +0100

    Updates

---
+++ static/js/main.0a9ee133.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{9:function(n,t,e){"use strict";e.r(t);var c=e(0),o=e.n(c),i=e(3),r=e.n(i),s=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,10)).then((function(t){var e=t.getCLS,c=t.getFID,o=t.getFCP,i=t.getLCP,r=t.getTTFB;e(n),c(n),o(n),i(n),r(n)}))},d=e(2);r.a.render(Object(d.jsx)(o.a.StrictMode,{children:Object(d.jsx)("h1",{children:"NEW WAY WEBSITE"})}),document.getElementById("root")),s()}},[[9,1,2]]]);
+//# sourceMappingURL=main.0a9ee133.chunk.js.map
